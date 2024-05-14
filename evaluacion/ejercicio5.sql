5ab92457bb2008ade46a711a9542a410ded902c7
Evelyn Ramirez <eveeramirez92@gmail.com>
2024-05-14 16:31:06 -0300

    ejercicio 5

--- evaluacion/ejercicio5.sql
+++ evaluacion/ejercicio5.sql
@@ -2,4 +2,4 @@
 -- de las mujeres que tienen licencia de conducir*/
 
 SELECT MAX(height), MIN(height), AVG(height) FROM drivers_license 
-WHERE gender LIKE '%female%'
+WHERE gender LIKE '%female%' 
